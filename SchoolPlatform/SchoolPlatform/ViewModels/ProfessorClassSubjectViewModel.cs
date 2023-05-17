96efcfd0b12a9be1fcb00d8760b2bd868baa5317
zsoltibv <zsoltibv@gmail.com>
2023-05-17 21:01:47 +0300

    Changed naming conventions

--- SchoolPlatform/SchoolPlatform/ViewModels/ProfessorClassSubjectViewModel.cs
+++ SchoolPlatform/SchoolPlatform/ViewModels/ProfessorClassSubjectViewModel.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
 
@@ -46,6 +47,9 @@ namespace SchoolPlatform.ViewModels
                 .ToList();
 
             Students = new ObservableCollection<Student>(matchingEntries);
+            if(Students.Count == 0) {
+                MessageBox.Show("No Students in this class");
+            }
         }
 
         //public ICommand SelectionChangedCommand => new RelayCommand<object>(RetrieveStudentList);
